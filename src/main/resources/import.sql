83c9f224f5a71a8bba7a839d24aee724e833df04
Ana Beatriz Marques Moreira <anamarques@unitins.br>
2023-08-20 15:46:58 -0300

    initial commit

---
+++ src/main/resources/import.sql
@@ -0,0 +1,11 @@
+-- This file allow to write SQL commands that will be emitted in test and dev.
+-- The commands are commented as their support depends of the database
+-- insert into myentity (id, field) values(1, 'field-1');
+-- insert into myentity (id, field) values(2, 'field-2');
+-- insert into myentity (id, field) values(3, 'field-3');
+-- alter sequence myentity_seq restart with 4;
+
+insert into oculos(id, referencia, cor, tamanho) values (1, 'C12A3', 'A2', '145');
+insert into oculos(id, referencia, cor, tamanho) values (2, 'B2023', 'C6', '145');
+insert into oculos(id, referencia, cor, tamanho) values (3, 'B2023', 'C5', '140');
+insert into oculos(id, referencia, cor, tamanho) values (4, 'A62G3', 'B2', '50');
